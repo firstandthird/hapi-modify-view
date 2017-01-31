1b925aa03594f7c8058d2bd5f5ee367ffa0fdcae
chris <orthagonal1@gmail.com>
2017-01-30 21:37:46 -0600

    use str2fn

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 'use strict';
-
+const str2fn = require('str2fn');
 exports.register = (server, options, allDone) => {
   server.ext('onPostHandler', (request, reply) => {
-    const modifyHtml = (typeof options.method === 'string') ? server.methods[options.method] : options.method;
     const source = request.response.source;
     request.render(source.template, source.context, source.options, (err, html) => {
       if (err) {
         return server.log(err);
       }
-      return reply(modifyHtml(html));
+      return reply(str2fn(server.methods, options.method)(html));
     });
   });
   allDone();
